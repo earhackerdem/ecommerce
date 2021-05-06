aab8579a3f70e1709c7a417a1eff65841bbae6ac
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2021-05-06 11:05:28 -0500

    6 - Modelo físico

---
+++ database/migrations/2021_05_05_224059_create_products_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug');
+            $table->text('description');
+            $table->float('price');
+
+            $table->foreignId('subcategory_id')->constrained();
+            $table->foreignId('brand_id')->constrained();
+
+            $table->integer('quantity');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
