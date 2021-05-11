b4f17879d7f8618429e91d33f5539ac965c29469
Earvin Saúl Pérez Ramos <earhackerdem@gmailcom>
2021-05-10 20:57:36 -0500

    10 - Insertar registros en la tabla brands

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -10,4 +10,20 @@ class Brand extends Model
     use HasFactory;
 
     protected $fillable = ['name'];
+
+    //Relación uno a muchos
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
+
+    //Relación muchos a muchos
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class);
+    }
+
+
 }
