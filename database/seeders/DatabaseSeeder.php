00908457afb6932fb1537995076821f3cacd28bf
Earvin Saúl Pérez Ramos <earhackerdem@gmailcom>
2021-05-18 17:04:44 -0500

    14 - Descargar imágenes para los productos

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,9 +18,11 @@ class DatabaseSeeder extends Seeder
     {
         Storage::deleteDirectory('subcategories');
         Storage::deleteDirectory('categories');
+        Storage::deleteDirectory('products');
 
         Storage::makeDirectory('categories');
         Storage::makeDirectory('subcategories');
+        Storage::makeDirectory('products');
 
         $this->call(UserSeeder::class);
         $this->call(CategorySeeder::class);
