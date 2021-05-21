8e928343ca2255c4539924dee98f67e0ba8b07ed
Earvin Saúl Pérez Ramos <earhackerdem@gmailcom>
2021-05-21 12:50:45 -0500

    25 - Agregando funcionalidad al menu II

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -1,6 +1,6 @@
 <header class="bg-trueGray-700 sticky top-0" x-data="dropdown()">
     <div class="container flex items-center h-16">
-        <a
+        <a :class="{'bg-opacity-100 text-orange-500' : open}"
         x-on:click="show()"
         class="flex flex-col items-center justify-center px-4 bg-white bg-opacity-25 text-white cursor-pointer font-semibold h-full"
             >
@@ -8,7 +8,7 @@
                 <path class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                     d="M4 6h16M4 12h16M4 18h16" />
             </svg>
-            <span>Categorías</span>
+            <span class="text-sm">Categorías</span>
         </a>
 
         <a href="/" class="mx-6">
