aa7c89b498def6b0a2ef3dc7a3dfdfdb0f158a88
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2021-05-27 11:29:04 -0500

    Traer  colores de productos

--- app/Http/Livewire/AddCartItem.php
+++ app/Http/Livewire/AddCartItem.php
@@ -2,11 +2,19 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Product;
 use Livewire\Component;
 
 class AddCartItem extends Component
 {
+    public $product,$quantity;
+
     public $qty = 1;
+
+    public function mount(Product $product)
+    {
+        $this->quantity = $this->product->quantity;
+    }
 
     public function decrement()
     {
