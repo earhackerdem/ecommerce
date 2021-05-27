87a2d7f9c3d010f8854794b6eeb1a4b5003b1c4e
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2021-05-27 18:01:01 -0500

    61 - Agregar item con talla de carrito

--- app/Http/Livewire/AddCartItemSize.php
+++ app/Http/Livewire/AddCartItemSize.php
@@ -3,6 +3,8 @@
 namespace App\Http\Livewire;
 
 use App\Models\Size;
+use Gloudemans\Shoppingcart\Facades\Cart;
+use Illuminate\Support\Facades\Storage;
 use Livewire\Component;
 
 class AddCartItemSize extends Component
@@ -20,21 +22,51 @@ class AddCartItemSize extends Component
 
     public $colors = [];
 
+    public $options = [];
+
+    public function mount()
+    {
+        $this->sizes = $this->product->sizes;
+        $this->options['image'] = Storage::url($this->product->images()->first()->url);
+    }
+
     public function updatedSizeId($value)
     {
         $size = Size::find($value);
         $this->colors = $size->colors;
+        $this->options['size'] = $size->name;
     }
 
     public function updatedColorId($value)
     {
         $size = Size::find($this->size_id);
-        $this->quantity = $size->colors->find($value)->pivot->quantity;
+        $color = $size->colors->find($value);
+        $this->quantity = $color->pivot->quantity;
+        $this->options['color'] = $color->name;
     }
 
-    public function mount()
+    public function decrement()
     {
-        $this->sizes = $this->product->sizes;
+        $this->qty = $this->qty - 1;
+    }
+
+    public function increment()
+    {
+        $this->qty = $this->qty + 1;
+    }
+
+    public function addItem()
+    {
+        Cart::add(['id' => $this->product->id,
+         'name' => $this->product->name,
+         'qty' => $this->qty,
+         'price' => $this->product->price,
+         'weight' => 550,
+         'options' => $this->options
+         ]);
+
+         $this->emitTo('dropdown-cart','render');
+
     }
 
     public function render()
