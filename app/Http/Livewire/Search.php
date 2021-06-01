c8ff713e4b11b524cb64cb94cc04d3ccf12a1381
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2021-06-01 16:50:06 -0500

    68 - Diseñar buscador

--- app/Http/Livewire/Search.php
+++ app/Http/Livewire/Search.php
@@ -10,10 +10,29 @@ class Search extends Component
 
     public $search;
 
+    public $open = false;
+
+    public function updatedSearch($value)
+    {
+        if($value){
+            $this->open = true;
+        }else{
+            $this->open = false;
+        }
+    }
+
     public function render()
     {
 
-        $products = Product::where('name','LIKE','%' . $this->search . '%')->get();
+        if ($this->search) {
+            $products = Product::where('name','LIKE','%' . $this->search . '%')
+            ->where('status',2)
+            ->take(8)
+            ->get();
+        } else {
+            $products = [];
+        }
+
 
         return view('livewire.search',compact('products'));
     }
